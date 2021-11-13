cadcbc9b87d243730f4e315caf469324bf3d05a3
Divya Prakash Gupta <77239774+divyakumarraj@users.noreply.github.com>
2021-11-13 15:12:48 +0530

    Create Apple.java
    Q3. Given a string, return a new string made of n copies of the first 2 chars of the original string where n is the length of the string. The string may be any length. If there are fewer than 2 chars, use whatever is there.
    If the input is “Apple” then the output should be “ApApApApAp”

---
+++ Apple.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class Apple {
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        String a;
+        a=sc.next();
+//        String str= "apple";
+        for(int i=0;i<4;i++){
+
+
+        if(a.length()>2)
+
+        {
+            System.out.print(a.substring(0,2));
+        }}
+
+
+    }
+}
