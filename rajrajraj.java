8c478b709d9bd3b268303d571a2633600fcdfb74
Divya Prakash Gupta <77239774+divyakumarraj@users.noreply.github.com>
2021-11-13 16:32:55 +0530

    Create rajrajraj.java
    Q5. Given a string and an int n, return a string made of n repetitions of the last n characters of the string. You may assume that n is between 0 and the length of the string, inclusive. For example, if the inputs are “Niraj” and 3, then the output should be “rajrajraj.

---
+++ rajrajraj.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class rajrajraj {
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        String a;
+        a=sc.next();
+        Scanner sa= new Scanner(System.in);
+        int b;
+        b=sc.nextInt();
+        for(int i=0;i<b;i++){
+        {
+            System.out.print(a.substring(a.length()-b));
+        }
+    }
+}
+}
